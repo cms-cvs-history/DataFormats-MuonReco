5650bfe6987ff29efe02be283282e0dce3624dfa
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2013-01-24 18:13:04 +0000

    add shared segments selector

--- interface/MuonSelectors.h
+++ interface/MuonSelectors.h
@@ -5,7 +5,7 @@
 // 
 //
 // Original Author:  Jake Ribnik, Dmytro Kovalskyi
-// $Id: MuonSelectors.h,v 1.14.6.1 2013/01/17 11:12:38 bellan Exp $
+// $Id: MuonSelectors.h,v 1.14.6.2 2013/01/17 17:58:22 bellan Exp $
 
 #include "DataFormats/MuonReco/interface/Muon.h"
 #include "TMath.h"
@@ -108,5 +108,9 @@ namespace muon {
    bool overlap( const reco::Muon& muon1, const reco::Muon& muon2, 
 		 double pullX = 1.0, double pullY = 1.0, bool checkAdjacentChambers = false);
 
+   /// Determine the number of shared segments between two muons.
+   /// Comparison is done using the segment references in the reco::Muon object.
+   int sharedSegments( const reco::Muon& muon1, const reco::Muon& muon2, 
+                       unsigned int segmentArbitrationMask = reco::MuonSegmentMatch::BestInChamberByDR ) ;
 }
 #endif
